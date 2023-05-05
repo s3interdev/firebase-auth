9c7ee59d82139b2e4b8ef3f2b0440f80b35f5c00
Michael Owuor <michael.owuor@gmail.com>
2023-05-05 21:07:42 +0300

    feat: html template setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	content: ['./index.html', './node_modules/flowbite/**/*.js'],
 	theme: {
 		extend: {},
 		fontFamily: {
 			ubuntu: 'Ubuntu, sans-serif',
 		},
 	},
-	plugins: [],
+	plugins: [require('flowbite/plugin')],
 };
